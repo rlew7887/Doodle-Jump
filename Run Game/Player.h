326581123b979f84231a49395aa2f1958571e795
Irene7044 <yen-ling@Irenegg>
2024-10-03 22:14:53 +0930

    Sorting files into folders

---
+++ Run Game/Player.h
@@ -0,0 +1,26 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
+
+class Player {
+  private:
+    RectangleShape defaultPlayer;
+
+  public:  
+    Player();
+
+    void moveLeft(RectangleShape& rect);
+
+    void moveRight(RectangleShape& rect);
+
+    void jump(RectangleShape& rect, float& velocity, float FPS);
+
+    void render();
+
+    ~Player() {}
+
+};
+
+#endif
