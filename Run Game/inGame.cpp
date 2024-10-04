58113320ffec55e877b6f8d0b6c4ae57b9ad4b8b
rlew7887 <rlew7887@LAPTOP-NGV5VERG>
2024-10-04 12:16:16 +0930

    Modify inGame

--- Run Game/inGame.cpp
+++ Run Game/inGame.cpp
@@ -1,6 +1,12 @@
 #include "inGame.h"
 #include <iostream>
 
+void inGame::gameOver(){
+    RenderWindow window(VideoMode(500, 800), "Doodle Jump");
+}
+
+void inGame::countScore(){}
+
 int inGame::getScore(){
     return position.y;
 }
