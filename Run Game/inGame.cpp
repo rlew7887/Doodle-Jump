1d3c51d7aa79a4c5621b50c9509677a6ec3b0132
rlew7887 <rlew7887@LAPTOP-NGV5VERG>
2024-10-04 16:45:01 +0930

    Added background image

--- Run Game/inGame.cpp
+++ Run Game/inGame.cpp
@@ -4,11 +4,33 @@
 inGame::inGame():score(score){}
 
 void inGame::gameOver(){
-    RenderWindow window(VideoMode(500, 800), "Doodle Jump");
+    RenderWindow window(VideoMode(500, 800), "Game Over");
+    sf::Texture t;
+    t.loadFromFile("grid-bg.jpg");
+    sf::Sprite s(t);
+    sf::Vector2u windowSize = window.getSize(); //get window size
+    sf::Vector2u textureSize = t.getSize(); //get image size
+
+    //Calculate scale factors to make image fit to window
+    float scaleX = (1.0f * windowSize.x) / textureSize.x;
+    float scaleY = (1.0f * windowSize.y) / textureSize.y;
+
+    s.setScale(scaleX, scaleY); //scale the sprite
+    while (window.isOpen()){
+        Event event;
+        while (window.pollEvent(event)){
+                if (event.type == Event::Closed)
+                    window.close();
+        }
+        window.clear();
+        window.draw(s);
+        window.display();
+    }
 }
 
 void inGame::countScore(){
-    //score = gety();
+    //if (Y > previousY)
+    //score ++;
 }
 
 int inGame::getScore(){
@@ -20,6 +42,14 @@ void inGame::render(){
     sf::Texture t;
     t.loadFromFile("grid-bg.jpg");
     sf::Sprite s(t);
+    sf::Vector2u windowSize = window.getSize(); //get window size
+    sf::Vector2u textureSize = t.getSize(); //get image size
+
+    //Calculate scale factors to make image fit to window
+    float scaleX = (1.0f * windowSize.x) / textureSize.x;
+    float scaleY = (1.0f * windowSize.y) / textureSize.y;
+
+    s.setScale(scaleX, scaleY); //scale the sprite
     // Player player;
     // player.render(); This function in player.cpp will be changed to just render a rectangle/character that can move left/right
     while (window.isOpen()){
