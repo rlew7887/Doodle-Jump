a23e7672bd91a34c03479a15876814c93bd374d1
rlew7887 <rlew7887@LAPTOP-NGV5VERG>
2024-10-11 13:07:51 +1030

    Moved files from Enemies folder to Run Game

--- Run Game/Enemies.h
+++ Run Game/Enemies.h
@@ -1,14 +1,20 @@
 #ifndef ENEMIES_H
 #define ENEMIES_H
+#include "Player.h"
+#include <SFML/Graphics.hpp>
+
+using namespace sf;
 
 class Enemies{
 protected:
     float X;
     float Y;
 public:
-    //virtual void onCollide(Player& player) = 0;
+    Enemies();
+    virtual void onCollide(Player& player) = 0;
     void render();
     void randomizeEnemies();
+    ~Enemies();
 };
 
 #endif
